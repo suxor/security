9e70e7bfdaae56a5be0625ade92164cc7332089e
suxor <sxq315@163.com>
2017-01-06 20:19:45 +0800

    save work

--- decrypt.c
+++ decrypt.c
@@ -94,4 +94,55 @@ int step_forward(int *key_index, int max_len, int base, int step)
     return 0;
 }
 
- 
+void *worker(void *args)
+{
+    int i = 0; 
+    int ret = 0;
+    struct crypt_data local_crypt_data;
+    worker_data_s *worker_data = (worker_data_s*)args;
+    char *curr_key = malloc(worker_data->max_len + 1);
+    assert(0 != curr_key);
+
+    memset(curr_key, 0, worker_data->max_len + 1);
+    local_crypt_data.initialized = 0;
+    
+    pthread_mutex_lock(&worker_data->mutex);
+    do {
+        index_to_password(worker_data->key_index, worker_data->max_len, curr_key);
+        for(i = 0; i < worker_data->count; i ++) {
+            DEBUG_OUTPUT(DEBUG_INFO, "[%p] current key is: %s\r\n", worker_data, curr_key);
+            if (0 == memcmp(expected_result, crypt_r(curr_key, curr_salt, &local_crypt_data), expected_length)) {
+                pthread_mutex_lock(&worker_data->main_data->mutex);
+                worker_data->main_data->key_was_got = 1;
+                strncpy(worker_data->main_data->result_key, curr_key, worker_data->max_len);
+                pthread_mutex_unlock(&worker_data->main_data->mutex);
+                break;
+            }
+            ret = next_password(worker_data->key_index, worker_data->max_len, dic_len, curr_key);
+            if (0 != ret) printf("[%p]get next password failed when key is: %s\r\n", worker_data, curr_key);
+        }
+
+        DEBUG_OUTPUT(DEBUG_WARNING, "[%p] complete current area.\r\n", worker_data);
+        pthread_mutex_lock(&worker_data->main_data->mutex);
+        if (worker_data->main_data->is_completed || worker_data->main_data->key_was_got) {
+            pthread_mutex_unlock(&worker_data->main_data->mutex);
+            break;
+        }
+        worker_data->main_data->worker_events[worker_data->id] = 1;
+        pthread_signal(&worker_data->main_data->cond);
+        DEBUG_OUTPUT(DEBUG_WARNING, "notify to main\r\n");
+        pthread_mutex_unlock(&worker_data->main_data->mutex);
+
+        pthread_cond_wait(&worker_data->cond, &worker_data->mutex);
+        DEBUG_OUTPUT(DEBUG_WARNING, "child get cond\r\n");
+    } while(worker_data->running);
+    pthread_mutex_unlock(&worker_data->mutex);
+
+    free(curr_key);
+    pthread_mutex_lock(&worker_data->main_data->mutex);
+    worker_data->main_data->active_worker_num --;
+    pthread_cond_signal(&worker_data->main_data->cond);
+    pthread_mutex_unlock(&worker_data->main_data->mutex);
+    return 0;
+}
+              
